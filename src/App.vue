062a00dcc86a681ec9bd90e458bec803dd69f46d
Ricardo Romo <jricardoromo21@gmail.com>
2018-08-12 23:48:41 -0500

    page

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo-white.jpg')
+    h1 RicardoMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    spinner(v-show="loading")
+    ul
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import getArtists from './api'
+import spinner from './components/spinner'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      loading:true,
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina' },
+        { name: 'Colombia', value: 'colombia' },
+        { name: 'España', value: 'spain' },
+      ],
+      selectedCountry: 'argentina'
+    }
+  },
+  components: {
+    Artist,
+    spinner
+  },
+  methods: {
+    refreshArtists() {
+      this.loading = true
+      this.artist = []
+      const self = this
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.artists = artists
+          self.loading=false
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry() {
+      this.refreshArtists()
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+  a
+    color black
+  </style>
---
+++ src/api/config.js
@@ -0,0 +1,10 @@
+// Application name	ricardomusic
+// API key	46c895155053a1a3db89386262d7a1e9
+// Shared secret	a5ea7ee58d7fe51b1b94ad37bfe5d560
+// Registered to	jricardoromo
+export default {
+  appname: 'ricardomusic',
+  apiKey: '46c895155053a1a3db89386262d7a1e9',
+  secret: 'a5ea7ee58d7fe51b1b94ad37bfe5d560',
+  registeredTo: 'jricardoromo'
+}
